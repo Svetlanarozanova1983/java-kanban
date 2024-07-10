9174ef1036308500d99b1ddce5feb5aebb8ba1a4
Svetlana Rozanova <rozanovasa@yandex.ru>
2024-07-10 12:33:41 +0300

    6_Sprint Fix test errors v6

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -7,36 +7,42 @@ public interface TaskManager {
 
 
     Task creationTask(Task input);
+
     Subtask creationSubtask(Subtask input);
+
     Epic creationEpic(Epic input);
 
-
     Task getTaskById(Integer id);
+
     Subtask getSubtaskById(Integer id);
+
     Epic getEpicById(Integer id);
 
-
     Task updateTask(Task input);
+
     Subtask updateSubtask(Subtask input);
+
     Epic updateEpic(Epic input);
 
-
     Collection<Task> getTasks();
+
     Collection<Subtask> getSubtasks();
+
     Collection<Epic> getEpics();
+
     Collection<Subtask> getSubtasksByEpicId(Integer id);
 
-
     boolean removeTaskById(Integer id);
+
     boolean removeSubtaskById(Integer id);
+
     boolean removeEpicById(Integer id);
 
-
     void clearTasks();
+
     void clearSubtasks();
+
     void clearEpics();
 
-
     List<Task> getHistory();
-
 }
