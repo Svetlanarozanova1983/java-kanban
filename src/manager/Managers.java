4451f70bfd66659a63abc0b7240a4350421f1057
Svetlana Rozanova <rozanovasa@yandex.ru>
2024-07-19 18:57:07 +0300

    7_Sprint Loading and Saving File added Unit tests v3

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -3,12 +3,10 @@ import java.io.File;
 public class Managers {
 
     private Managers() {
-
     }
-    private final static String PATH_TO_FILE = "/Users/svetlana/work/IdeaProjects/java-kanban/file_data.db";
 
     public static TaskManager getDefault() {
-        return FileBackedTaskManager.loadFromFile(new File(PATH_TO_FILE));
+        return FileBackedTaskManager.loadFromFile(new File("/Users/svetlana/work/IdeaProjects/java-kanban/file_data.db"));
     }
 
     public static HistoryManager getDefaultHistory() {
