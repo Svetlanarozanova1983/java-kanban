3736c07daf2c09136e64967fadfbf4d04fa42f22
Svetlana Rozanova <rozanovasa@yandex.ru>
2024-08-07 08:19:47 +0300

    8_Sprint StartTime and Duration added Unit tests v6

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -3,7 +3,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class Epic extends Task {
 
@@ -45,9 +44,9 @@ public class Epic extends Task {
         var startTime = subtasks
                         .stream()
                         .filter(f -> f.getStartTime() != null)
-                        .mapToLong(s ->  s.getStartTime().toEpochSecond(zone) )
+                        .mapToLong(s ->  s.getStartTime().toEpochSecond(zone))
                         .reduce(Long::min);
-        if(!startTime.isEmpty()) {
+        if (!startTime.isEmpty()) {
             LocalDateTime result = LocalDateTime.ofEpochSecond(startTime.getAsLong(), 0, zone);
             return result;
         }
@@ -81,9 +80,9 @@ public class Epic extends Task {
     @Override
     public String toString() {
         var dur = getDuration();
-        var durStr = dur!= null ?  String.valueOf(dur.toMinutes()) : "";
+        var durStr = dur != null ?  String.valueOf(dur.toMinutes()) : "";
         var start = getStartTime();
-        var startStr = start!= null ?  String.valueOf(start) : "";
+        var startStr = start != null ?  String.valueOf(start) : "";
         return getId() + ",epic," + getName() + "," + getStatus() + "," + getDescription() + "," + startStr + "," + durStr + ",";
     }
 }
